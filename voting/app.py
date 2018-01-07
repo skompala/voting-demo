53ad38ab4aa43f15f9a4b075b9344acecdc1eec9
skompala <smanyam.kompala@gmail.com>
2018-01-07 09:49:22 -0500

    test

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "hadoop"
+optionB = "spark"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
